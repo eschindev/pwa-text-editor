14e885a1e5fdeb7511fbe470a63ad22e72cc3208
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-25 10:01:18 -0700

    built out src-sw.js -- still incomplete

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -19,12 +19,38 @@ const pageCache = new CacheFirst({
   ],
 });
 
+offlineFallback({
+  cacheName: "offline-cache",
+  fallbackHTML: "<h1>You are offline</h1>",
+  fallbacks: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      response: "<img src='/offline-image.png'>",
+    },
+    {
+      urlPattern: /\.(?:js|css)$/,
+      response: "",
+    },
+  ],
+});
+
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === "style" || request.destination === "script",
+  new CacheFirst({
+    cacheName: "assets-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
